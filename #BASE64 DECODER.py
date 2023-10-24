249ee0bcbb4b00b555e633b04b898abd37d9c296
Reg <109710591+Condor6969@users.noreply.github.com>
2023-10-24 11:27:27 -0500

    Update #BASE64 DECODER.py

--- #BASE64 DECODER.py
+++ #BASE64 DECODER.py
@@ -3,20 +3,20 @@
 import base64
 
 
-# Function to encode data to base64
+# Encode
 def base64_encoder(data):
     encoded_bytes = base64.b64encode(data.encode('utf-8'))
     encoded_str = encoded_bytes.decode('utf-8')
     return encoded_str
 
-# Function to decode data from base64
+# Decode
 def base64_decoder(encoded_str):
     encoded_bytes = encoded_str.encode('utf-8')
     decoded_bytes = base64.b64decode(encoded_bytes)
     decoded_str = decoded_bytes.decode('utf-8')
     return decoded_str
 
-# Example usage
+# put your data to be encoded after data on the first line
 data = "773719064618467329" # data to be encoded
 encoded_data = base64_encoder(data)
 print("Encoded data:", encoded_data)
